b87b15825cbb440848a087f944ee0fd6b1ea5755
guillermo-rdz <nemesis_rmgj@hotmail.com>
2016-03-08 16:49:23 -0600

    Save_db Edit
    Guardar registros en la base de datos

--- app/model/save_db.php
+++ app/model/save_db.php
@@ -16,18 +16,33 @@
     	 public function save_Volatile($objeto){
 
 //--------------------------------Ingreso de datos a la base de datos de la tabla volatil---------------------------
-    	 	/*for ($i=0; $i < count($objeto); $i++) { 
-				$eventTime = $objeto[$i]->vid;
-				echo $eventTime;
-			}*/
-			echo $objeto[0]->vid;
-		    	/*
-		    	if ($this->mysqli->query("INSERT INTO prueba2 VALUES (default,'$name','$arreglo_trama[0]', '$arreglo_trama[1]', '$arreglo_trama[2]')")) {
-					echo "Se ingresaron los registros";
-				}
-				else{
-					echo "No se ingresaron los registros";
-				}*/
+
+			//echo $objeto->vid;
+			echo $vid = $objeto->vid;
+			echo $imei = $objeto->imei;
+			echo $lat = $objeto->lat;
+			echo $lon = $objeto->lon;
+			//$eventTime = $objeto->eventTime;
+			//$eventTime = date("Y-m-d H:i:s");
+			$eventTime = $objeto->eventTime;
+			$tx = $objeto->tx;
+			if (empty($tx)) {
+				$tx = "0,0,0,0,0";
+			}
+			//$tx = "0,1,2,3,4";
+			$trama = explode(",", $tx);
+			$up = $trama[0];
+			$down = $trama[1];
+			$abord = $trama[2];
+			$false_up = $trama[3];
+			$false_down = $trama[4];
+
+		    	
+		    	//$this->mysqli->query("INSERT INTO data_frame2 VALUES (default, '$up', '$down', '$abord', '$false_up', '$false_down', '$eventTime', '$lat', '$lon', '$imei', '$vid'");
+		    	$this->mysqli->query("INSERT INTO data_frame2 VALUES (default, '$up', '$down', '$abord', '$false_up', '$false_down', '$eventTime', '$lat', '$lon', '$imei', '$vid')");
+		    	//$this->mysqli->query("INSERT INTO data_frame2 VALUES (default, ".$up.", ".$down.", ".$abord.", ".$false_up.", ".$false_down.", ".$eventTime.", ".$lat.", ".$lon.", ".$imei.", ".$vid."");
+					//echo "Se ingresaron los registros";
+				
 
     	 }
 
