a1941600f2516c55bd534aba172f3d82ac9229bb
guillermo-rdz <nemesis_rmgj@hotmail.com>
2016-03-10 16:59:45 -0600

    save_db Edit
    Json en cadena

--- app/model/save_db.php
+++ app/model/save_db.php
@@ -1,14 +1,14 @@
 <?php 
-	$json_l = $_POST['liveData'];//------------------ Objeto de eventos ----------------------
-	$json_v = $_POST['vehiculos'];//----------------- Objeto de Vehiculos --------------------
+	//$json_l = $_POST['liveData'];//------------------ Objeto de eventos ----------------------
+	//$json_v = $_POST['vehiculos'];//----------------- Objeto de Vehiculos --------------------
 	$json_id = $_POST['vid'];//---------------------- Id de los vehiculos -----------------------
 
 	require_once "conexion.php";
 
 
 	//$events = json_decode($json_l, false, 512, JSON_BIGINT_AS_STRING);
-	$vehicles = json_decode($json_v, false, 512, JSON_BIGINT_AS_STRING);
-	//$vid = json_decode($json_id, false, 512, JSON_BIGINT_AS_STRING);
+	//$vehicles = json_decode($json_v, false, 512, JSON_BIGINT_AS_STRING);
+	$vid = json_decode($json_id, false, 512, JSON_BIGINT_AS_STRING);
 
 
     class Data_frame extends conexion{
@@ -35,15 +35,16 @@
 			}
 			else{
 
-				//$tx = "0,1,2,3,4";
+				//$tx = "0,1,2,3,4,5";
 				$trama = explode(",", $tx);
 				$up = $trama[0];
 				$down = $trama[1];
 				$abord = $trama[2];
 				$false_up = $trama[3];
 				$false_down = $trama[4];
+				$error = 0;
 
-				if ($this->mysqli->query("INSERT INTO data_frame VALUES (default, '$up', '$down', '$abord', '$false_up', '$false_down', '$eventTime', '$lat', '$lon', '$imei', '$vid')")) {
+				if ($this->mysqli->query("INSERT INTO data_frame VALUES (default, '$up', '$down', '$abord', '$false_up', '$false_down', '$error', '$eventTime', '$lat', '$lon', '$imei', '$vid')")) {
 					echo "Se ingresaron los registros";
 				}
 				else{
@@ -57,17 +58,37 @@
 
 
     	 public function queryJson($vid){
-    	 	$rawdata = array();
-    	 	$i = 0;
+    	 	//$rawdata = array();
+    	 	//$i = 0;
     	 	//$query = $this->mysqli->query("SELECT * FROM data_frame2 WHERE '$vid'=106");
-    	 	$query = $this->mysqli->query("SELECT vehicle2.name_vehicle, data_frame2.up, data_frame2.down, data_frame2.aboart,data_frame2.false_up, data_frame2.false_down, data_frame2.event_date, data_frame2.lat, data_frame2.lon from data_frame2 INNER JOIN vehicle2 on data_frame2.vehicle_idvehicle = vehicle2.idvehicle where vehicle2.idvehicle = 106 order by event_date");
+    	 	$query = $this->mysqli->query("SELECT vehicles.name_vehicle, data_frame.up, data_frame.down, data_frame.aboart,data_frame.false_up, data_frame.false_down, data_frame.event_date, data_frame.lat, data_frame.lon from data_frame INNER JOIN vehicles on data_frame.vehicle_idvehicle = vehicles.idvehicle where vehicles.idvehicle = '$vid' order by event_date");
+    	 	/*while ($row = $query->fetch_array()) {
+    	 		$name = $row["name_vehicle"];
+
+    	 		$arreglo[] = array('name_vehicle' => $name );
+    	 	}
+    	 	$json_string = json_encode($arreglo);
+    	 	print_r($json_string);*/
+    	 	$output = '{"data":[';
     	 	while ($row = $query->fetch_array()) {
-    	 		$rawdata[$i] = $row;
-    	 		$i++;
+    	 		//$rawdata[$i] = $row;
+    	 		//$i++;
+    	 		if ($output!='{"data":[') {$output .= ",";}
+    	 		$output .= '{"Name_Vehicle":"'.$row["name_vehicle"].'",';
+    	 		$output .= '"Up":"'.$row["up"].'",';
+    	 		$output .= '"Down":"'.$row["down"].'",';
+    	 		$output .= '"Abord":"'.$row["aboart"].'",';
+    	 		$output .= '"False_Up":"'.$row["false_up"].'",';
+    	 		$output .= '"False_Down":"'.$row["false_down"].'",';
+    	 		$output .= '"Event_Date":"'.$row["event_date"].'",';
+    	 		$output .= '"Lat":"'.$row["lat"].'",';
+    	 		$output .= '"Lon":"'.$row["lon"].'"}';
     	 	}
 
     	 	//print_r($rawdata);
-    	 	echo json_encode($rawdata);
+    	 	$output .= "]}";
+    	 	echo $output;
+    	 	//print_r($output);
     	 }
 
     	 public function save_Vehicles($vehicles){
@@ -89,8 +110,8 @@
 
     }
     $instance = new Data_frame();
-	$instance->save_Volatile($events);
+	//$instance->save_Volatile($events);
 	$instance->queryJson($vid);
-	$instance->save_Vehicles($vehicles);
+	//$instance->save_Vehicles($vehicles);
 
  ?>
